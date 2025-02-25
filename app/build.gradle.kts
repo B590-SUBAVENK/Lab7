953e4b17988b6bc14bc330e998ff4f5ac10db928
subavenk <subavenk@iu.edu>
2025-02-25 16:59:43 -0500

    Homework3 Part2 - Completed till Slide 38

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation ("androidx.fragment:fragment-ktx:1.6.2")
 }
