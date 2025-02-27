01e1c139854730ad04d75097dd2fc69b0462a146
subavenk <subavenk@iu.edu>
2025-02-27 01:09:14 -0500

    Homework3 Part9 - Completed

--- app/src/main/java/com/iub/lab7/database/TicketDatabase.kt
+++ app/src/main/java/com/iub/lab7/database/TicketDatabase.kt
@@ -3,11 +3,21 @@ package com.iub.lab7.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.iub.lab7.database.TicketTypeConverter
 import com.iub.lab7.Ticket
 
-@Database(entities = [Ticket::class], version = 1)
+@Database(entities = [Ticket::class], version = 2)
 @TypeConverters(TicketTypeConverter::class)
 abstract class TicketDatabase : RoomDatabase() {
     abstract fun ticketDao(): TicketDao
+}
+
+val migration_1_2 = object : Migration(1, 2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL(
+            "ALTER TABLE Ticket ADD COLUMN assignee TEXT NOT NULL DEFAULT ''"
+        )
+    }
 }
